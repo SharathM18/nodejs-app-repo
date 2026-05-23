a9ee471e4a9813f9b2290f7699040302917e0811
Sharath M <sharathm5684@gmail.com>
2026-05-23 16:17:19 +0530

    changedsdl

--- src/test-secret.ts
+++ src/test-secret.ts
@@ -2,5 +2,3 @@ export const awsConfig = {
   accessKeyId: 'AKIAIOSFODNN7EXAMPLE',
   secretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
 };
-
-export const githubToken = 'ghp_123456789012345678901234567890123456';
